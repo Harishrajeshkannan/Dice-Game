0cebce8d5e2450a7d41b3bfe1d63732771172e34
Harishrajeshkannan <117362765+Harishrajeshkannan@users.noreply.github.com>
2025-02-20 20:46:14 +0530

    index.js

--- index.js
+++ index.js
@@ -4,54 +4,10 @@ function roll()
 {
     var val1=Math.floor(Math.random()*6)+1;
     var val2=Math.floor(Math.random()*6)+1;
-    if(val1==1)
-    {
-        document.querySelector("#dice1").setAttribute("src","images/dice1.png");
-    }
-    else if(val1==2)
-    {
-        document.querySelector("#dice1").setAttribute("src","images/dice2.png");
-    }
-    else if(val1==3)
-    {
-        document.querySelector("#dice1").setAttribute("src","images/dice3.png");
-    }
-    else if(val1==4)
-    {
-        document.querySelector("#dice1").setAttribute("src","images/dice4.png");
-    }
-    else if(val1==5)
-    {
-        document.querySelector("#dice1").setAttribute("src","images/dice5.png");
-    }
-    else if(val1==6)
-    {
-        document.querySelector("#dice1").setAttribute("src","images/dice6.png");
-    }
-    if(val2==1)
-    {
-        document.querySelector("#dice2").setAttribute("src","images/dice1.png");
-    }
-    else if(val2==2)
-    {
-        document.querySelector("#dice2").setAttribute("src","images/dice2.png");
-    }
-    else if(val2==3)
-    {
-        document.querySelector("#dice2").setAttribute("src","images/dice3.png");
-    }
-    else if(val2==4)
-    {
-        document.querySelector("#dice2").setAttribute("src","images/dice4.png");
-    }
-    else if(val2==5)
-    {
-       document.querySelector("#dice2").setAttribute("src","images/dice5.png");
-    }
-    else if(val2==6)
-    {
-        document.querySelector("#dice2").setAttribute("src","images/dice6.png");
-    }
+    var dice1="dice"+val1+".png";
+    var dice2="dice"+val2+".png";
+    document.querySelector("#dice1").setAttribute("src","images/"+dice1);    
+    document.querySelector("#dice2").setAttribute("src","images/"+dice2);
     findWinner(val1,val2);
 }
 
